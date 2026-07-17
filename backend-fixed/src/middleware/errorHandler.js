4f54c16f06ebf374c6be2f8552b9f49ea66e1715
abdulsalam <olawoyinabadulsalam202@gmail.com>
2026-07-17 08:17:21 +0100

    Remove old backend-fixed folder

--- backend-fixed/src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// Centralized error handler. Keeps stray exceptions from leaking stack
-// traces to the client and gives the frontend a consistent { error } shape.
-function errorHandler(err, req, res, next) { // eslint-disable-line no-unused-vars
-  console.error(err);
-  if (res.headersSent) return next(err);
-  res.status(500).json({ error: 'Something went wrong on our end. Please try again.' });
-}
-
-module.exports = errorHandler;
--- backend-fixed/src/utils/id.js
+++
@@ -1,9 +0,0 @@
-const { nanoid } = require('nanoid');
-
-// Generates ids in the same shape the frontend already expects,
-// e.g. role_abc123, p_abc123, voter_abc123, vote_abc123, season_abc123
-function makeId(prefix) {
-  return `${prefix}_${nanoid(10)}`;
-}
-
-module.exports = { makeId };
